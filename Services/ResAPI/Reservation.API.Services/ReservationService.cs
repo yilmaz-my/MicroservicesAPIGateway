394fa1caf2bf93c155382581d08db4bc0c31e8b0
Your Name <you@example.com>
2022-07-03 19:52:54 +0300

    Microservices API Gateway

---
+++ Services/ResAPI/Reservation.API.Services/ReservationService.cs
@@ -0,0 +1,26 @@
+﻿using Reservation.API.Infrastructure;
+using Reservation.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservation.API.Services
+{
+    public class ReservationService : IReservationService
+    {
+        public ReservationDTO GetResByBkgNumber(int BkgNumber)
+        {
+            return new ReservationDTO()
+            {
+                Id = (new Random().Next(100)),
+                Amount = (new Random().Next(100)),
+                BkgDate = DateTime.Now,
+                CheckinDate = DateTime.Now.AddDays(30),
+                CheckoutDate = DateTime.Now.AddDays(37),
+                BkgNumber = BkgNumber
+            };
+        }
+    }
+}
---
+++ Services/ResAPI/Reservation.API/Controllers/ResController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reservation.API.Infrastructure;
+using Reservation.API.Models;
+
+namespace Reservation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResController : ControllerBase
+    {
+        private readonly IReservationService _reservationService;
+        public ResController(IReservationService reservationService)
+        {
+            _reservationService = reservationService;
+        }
+        [HttpGet("{Id}")]
+        public ReservationDTO Get(int Id)
+        {
+            return _reservationService.GetResByBkgNumber(Id);
+        }
+    }
+}
